6a45fb63b4a81a9cb4738f931532f988b91d1860
ftm1337 <ftm1337@protonmail.com>
2022-06-24 10:05:15 -0400

    Explore Pool on Nomics Mtvscout

--- src/components/PositionCard/index.tsx
+++ src/components/PositionCard/index.tsx
@@ -228,10 +228,10 @@ export default function FullPositionCard({ pair, border }: PositionCardProps) {
 
             <AutoRow justify="center" marginTop={'10px'}>
               <ExternalLink href={`https://nomics.com/exchanges/vax/markets/${pair.liquidityToken.address}`}>
-              Explore Pool on: <img src={Nomics} alt="nomics.com/exchanges/vax/markets" width="14px" style={{ marginRight: '0.5rem' }} />Nomics↗
+              Explore Pool on: <img src={Nomics} alt="nomics.com/exchanges/vax/markets" width="14px" style={{ marginLeft: '0.5rem' }} />Nomics↗
               </ExternalLink>
               <ExternalLink href={`https://mtvscout.com/address/${pair.liquidityToken.address}/tokens#tokens`}>
-              <img src={MtvScout} alt="VAX@mtvscout" width="14px" style={{ marginRight: '0.5rem' }} />Mtvscout↗
+              <img src={MtvScout} alt="VAX@mtvscout" width="14px" style={{ marginLeft: '0.5rem' }} />Mtvscout↗
               </ExternalLink>
             </AutoRow>
 
