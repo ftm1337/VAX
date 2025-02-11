5ddddbdfed6cfa04742a134e4770cd27735ceb52
ftm1337 <84405345+ftm1337@users.noreply.github.com>
2025-02-10 21:43:54 -0500

    use rehosted tokenlist from ftm.guru

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -3,5 +3,6 @@ export const DEFAULT_TOKEN_LIST_URL = ''
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   //'https://raw.githubusercontent.com/mtvguru/vax-list/main/docs/vax-list.json',
-  'https://mtv.guru/vax-list/vax-list.json'
+  //'https://mtv.guru/vax-list/vax-list.json',
+  'https://ftm.guru/tokenlists/vax.json'
 ]
